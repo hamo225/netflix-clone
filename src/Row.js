c291c0f71b1ae8272da17821e1552bf783a6a5c2
Tarek Hamaoui <tarek.m.hamaoui@gmail.com>
2021-05-11 14:10:51 +0200

    sets up firebase deployment

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseurl = "https://image.tmdb.org/t/p/original";
 
@@ -8,6 +10,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
   //   state is like short term memory that disapears on refresh
   //   state syntax, the name, the set name and set to usestate with an initial value of an empty array
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
   //   now need to fill it with information
   //   now need code snippet that runs based on specific conditions/variable
   //   when the row loads we want something to run. This is what we use useEffect for. Something to happen on page load.
@@ -28,6 +31,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
   // when using useEffect - if we are using a variable from outside the block than we MUST pass it as a dependency
   //   if dependenice blank. then run once when row loads and do not run again. If we were to pass in a variable then it will reload once that dependencie changes
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+
+    console.log(movie?.name);
+  };
+
   return (
     <div className="row">
       {/* Title */}
@@ -44,11 +70,13 @@ function Row({ title, fetchUrl, isLargeRow }) {
             `}
             alt={`${baseurl}  ${movie.name} `}
             key={movie.id}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
 
-      {/* container -> posters */}
+      {/* takes video id and options */}
+      {trailerUrl && <Youtube videoId={`trailerUrl`} opts={opts}></Youtube>}
     </div>
   );
 }
